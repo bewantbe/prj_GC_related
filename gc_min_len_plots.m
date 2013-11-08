ece5641dc637794c5fb61e9846861a4f393d4455
xyy@xhp_Wheezy <xyy@xhp_Wheezy>
2013-11-08 16:18:17 +0800

    now generate gc_min_len_plotsa good plot

--- gc_min_len_plots.m
+++ gc_min_len_plots.m
@@ -1,26 +1,27 @@
 %
+pic_common_include;
 
 simu_time = 1e5;
 stv = 0.5;
 len = simu_time/stv;
-F1 = 0.157e-3;  % GC1: 1, 0.012, 0.01
-%F1 = 0.136e-3*2*stv;  % GC1: 0.24, 0.02, 0.01
-%F1 = 0.325e-3;  % GC1: 1, 0.006, 0.01
+F1 = 1.57e-4;  % GC1: 1, 0.012, 0.01
+%F1 = 1.36e-4;  % GC1: 0.24, 0.02, 0.01
+%F1 = 3.25e-4;  % GC1: 1, 0.006, 0.01
 F1 = F1*2*stv;
-gc_scale = 1e-3;  % plot GC in this scale
+gc_scale = 1e-4;  % plot GC in this scale
 
-m = 22;
+m = 17;
 c = len*F1;
 ncc_mean = m+c
 ncc_var  = 2*m + 4*c
 s_x = linspace(0, ncc_mean+3*sqrt(ncc_var), 5000);
 
-s_ncc  = chi2_pdf(s_x, m, c)*len*gc_scale;
+s_ncc1  = chi2_pdf(s_x, m, c)*len*gc_scale;
 s_ncc0 = chi2_pdf(s_x, m, 0)*len*gc_scale;
 s_x = s_x / len / gc_scale;
 
 % test
-p_val = 5e-2;
+p_val = 1e-2;
 gc_cut = chi2inv(1-p_val, m)/len
 %gc_cut = 0.275e-3;
 p_H0_significant = chi2_cdf(len*gc_cut, m, 0)
@@ -30,14 +31,22 @@ p_guess_correct = p_H0_significant * (1-p_H1_nonsignificant)
 
 % plots
 plot_mbar = @(m, c) line_mean_bar((m+c)/len/gc_scale, chi2_pdf(m+c, m, c)*len*gc_scale, sqrt(2*m+4*c)/len/gc_scale, 'color', 'red');
-%line_mean_bar(ncc_mean, chi2_pdf(ncc_mean, m, c), sqrt(ncc_var), 'color', 'red');
 
-figure(1);
-plot(s_x, s_ncc, s_x, s_ncc0);
-line(gc_cut/gc_scale*[1 1]', [0,1.1*chi2pdf(m-2,m)*len*gc_scale]', 'color','black');
-legend('GC1','GC0', sprintf('GC threshold - p = %.3f',p_val));
+figure(1);  clf; cla;
+hold on
+rg0 = s_x<gc_cut/gc_scale;
+rg1 = s_x>gc_cut/gc_scale;
+area(s_x(rg1), s_ncc1(rg1), 'facecolor', [0.68 0.79 1], 'linewidth', 0);
+area(s_x(rg0), s_ncc0(rg0), 'facecolor', [0.7 1 0.7], 'linewidth', 0);
+haxp = plot(s_x, s_ncc1, s_x, s_ncc0);
+hold off
+haxl = line(gc_cut/gc_scale*[1 1]', [0,1.1*chi2pdf(m-2,m)*len*gc_scale]', 'color','black');
+hd=legend(haxp, sprintf('GC1(F=%.2e)',F1), 'GC0');
+hd=legend(haxl, sprintf('GC_{thres}(p=%.3f)',p_val));
+set(hd, 'fontsize', fontsize-2);
 %plot_mbar(m, c);
 %plot_mbar(m, 0);
 xlabel(sprintf('GC / %.2e', gc_scale));
 ylabel('pdf');
+pic_output_color(sprintf('pdf_m=%d_L=%2.e_F0_F1=%.2e', m, len, F1));
 
