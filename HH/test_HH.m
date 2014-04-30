582b98bfe904b220eb985794d676bd5c9dfca9f3
xyy@xhp7 <xyy@xhp7>
2014-04-30 23:32:47 +0800

    HH_cal.nb: now more easier to use. test_HH pass all tests

--- HH/test_HH.m
+++ HH/test_HH.m
@@ -1,16 +1,35 @@
 %
 
 pm.net  = 'net_1_0';
-pm.net  = 'net_2_2';
 pm.ps   = 0.04;
-%pm.ps   = 0.00;
 pm.pr   = 1.6;
 pm.scee = 0.05;
 pm.t    = 40;
 pm.dt   = 2^-9;
 pm.stv  = 0.5;
-pm.extra_cmd = ['--save-poisson-events spe2.txt'];
 
+id_case = 2;
+switch id_case
+  case 0
+    % N=1, zeros input
+    pm.ps   = 0.00;
+    pm.net = 'net_1_0';
+    path_spe = 'spe0.txt';
+    path_mathe = 'tb0.txt';
+  case 1
+    % N=1, poisson
+    pm.net = 'net_1_0';
+    path_spe = 'spe1.txt';
+    path_mathe = 'tb1.txt';
+  case 2
+    % N=2, poisson
+    pm.net = 'net_2_2';
+    path_spe = 'spe2.txt';
+    path_mathe = 'tb2.txt';
+end
+
+%pm.extra_cmd = sprintf('--save-poisson-events %s',path_spe);
+pm.extra_cmd = sprintf('--RC-filter 0 1 --save-poisson-events %s',path_spe);
 [X, ISI, ras] = gen_HH(pm);
 
 rg_l = 80;
@@ -20,16 +39,16 @@ rg_e = rg_b + rg_l - 1;
 %fscaling = @(x) x*10-65;
 fscaling = @(x) x*10;
 
-X_e = load('tb2.txt')';
+X_e = load(path_mathe)';
 
 figure(1);
 plot(1:rg_l, fscaling(X(:,rg_b:rg_e)), '-o', 'markersize', 2,...
      1:rg_l, X_e(:,rg_b:rg_e), '-o', 'markersize', 2);
+if id_case==2
+  legend('rt-x','rt-y','ma-x','ma-y');
+else
+  legend('rt-x','ma-x');
+end
 
 figure(2);
 plot(1:rg_l, fscaling(X(:,rg_b:rg_e)) - X_e(:,rg_b:rg_e), '-o', 'markersize', 2);
-
-% passed:
-% N=1, zeros input
-
-% N=1, poisson
