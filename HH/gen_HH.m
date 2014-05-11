8c2d27d0ff89ad4c1a8dc1d3f19b3f0246874c62
xyy@xhp7 <xyy@xhp7>
2014-05-11 23:42:35 +0800

    refine HH/gen_HH.m, add many comment, portable to MATLAB; HH/HH_cal.nb make S^II more consist

--- HH/gen_HH.m
+++ HH/gen_HH.m
@@ -1,6 +1,38 @@
-% Generate HH neuron data by calling external raster_tuning_HH
+% Generate HH neuron data by calling raster_tuning_HH.exe
 % Will use preserved data automatically
+%
+%  [X, ISI, ras] = gen_HH(pm [, gen_cmd [, data_dir_prefix]])
+%
+% Usage example 1: % default means optional
+%  pm.net  = 'net_2_2';
+%  pm.ps   = 0.04;
+%  pm.pr   = 1.6;
+%  pm.scee = 0.05;
+%  pm.t    = 1e4;
+%  pm.In   = 0;          % default: 0. Number of Inhibitory neurons.
+%                        %             Indexes are later half
+%  pm.dt   = 1.0/32;     % default: 1/32
+%  pm.stv  = 0.5;        % default: 0.5
+%  pm.seed = 'auto';     % default: 'auto'(or []). Accept integers
+%  pm.scie = 0.00;       % default: 0. Strength from Ex. to In.
+%  pm.scei = 0.00;       % default: 0. Strength from In. to Ex.
+%  pm.scii = 0.00;       % default: 0.
+%  pm.extra_cmd = '--RC-filter 0 1'  % default: '--RC-filter 0 1'
+%                                    % put all other parameters here.
+%  [X, ISI, ras] = gen_HH(pm);
+%
+% Usage example 3: Always re-generate data, then read it
+%  X = gen_HH(pm, 'new');
+%
+% Usage example 2: Generate data if not exist, read it, then remove data files
+%  X = gen_HH(pm, 'rm');
+%
+% Other possible values for "gen_cmd":
+%  'read'   Read data files if exist, do nothing and return [] otherwise;
+%  'nameX'  return path to the voltage data file, instead of read it;
+%  'cmd'    Show command call to raster_tuning_HH, then exit.
 
+% Behaviour
 % gen_cmd   no data    have data   background(partial data)
 % ''        gen+read   read        gen+read(overwrite)
 % 'new'     gen+read   gen+read    gen+read(overwrite)
@@ -10,19 +42,12 @@
 % 'cmd'     print cmd for gen then exit
 % background  gen&     none        gen&
 
-% Usage example:
-%{
-  pm.net  = 'net_2_2';
-  pm.ps   = 0.04;
-  pm.pr   = 1.6;
-  pm.scee = 0.05;
-  pm.t    = 1e4;
-  pm.dt   = 1/32;
-  pm.stv  = 0.5;
-  [X, ISI, ras] = gen_HH(pm);
-%}
-
 function [X, ISI, ras] = gen_HH(pm, gen_cmd, data_dir_prefix)
+if nargin()==0
+    disp(' [X, ISI, ras] = gen_HH(pm [, gen_cmd [, data_dir_prefix]])');
+    disp(' Type "help gen_HH" for more help');
+    error('Lack of input parameters.');
+end
 X=[];
 ISI=[];
 ras=[];
@@ -43,15 +68,15 @@ while length(gen_cmd)>0
     [tok, gen_cmd] = strtok(gen_cmd, ' ,');
     switch tok
     case 'rm'
-        mode_rm_only = true;         % remove the output files and exit
+        mode_rm_only = true;    % Remove specific data files then exit
     case 'read'
-        mode_read_only = true;
+        mode_read_only = true;  % Read data files if there are, otherwise exit
     case 'new'
-        new_run = true;
+        new_run = true;         % Regenerate data and read it
     case 'nameX'
-        return_X_name = true;        % return name of X data
+        return_X_name = true;   % Return file path of voltage data file
     case 'cmd'
-        mode_show_cmd = true;
+        mode_show_cmd = true;   % Show the command to call, then exit
     otherwise
         error(sprintf('no this option: "%s"', tok));
     end
@@ -77,7 +102,7 @@ if ~isfield(pm, 'stv') || isempty(pm.stv)
     pm.stv = 0.5;
 end
 if ~isfield(pm, 'dt') || isempty(pm.dt)
-    pm.dt = 0.5;
+    pm.dt = 1.0/32;
 end
 if ~isfield(pm, 'scee') || isempty(pm.scee)
     pm.scee = 0;
@@ -105,7 +130,7 @@ st_sc = strrep(mat2str([pm.scee, pm.scei, pm.scie, pm.scii]),' ',',');
 st_p  = strrep(mat2str([pm.nE, pm.nI]),' ',',');
 file_inf_st =...
     sprintf('%s_p%s_sc=%s_pr=%g_ps=%g_t=%.2e_stv=%g',...
-            pm.net, st_p, st_sc, pm.pr, pm.ps, pm.t, pm.stv);
+            pm.net, st_p(2:end-1), st_sc(2:end-1), pm.pr, pm.ps, pm.t, pm.stv);
 if ~exist('data_dir_prefix', 'var')
     data_dir_prefix = ['data', filesep];
 end
@@ -116,15 +141,22 @@ output_RAS_name = [file_prefix, 'RAS_', file_inf_st,'.txt'];
 
 % construct command string
 pathdir = fileparts(mfilename('fullpath'));
+% ! NOTE: pm.scie is strength from Ex. to In. type
+%         Seems biologist love this convention, I have no idea.
 st_neu_s =...
     sprintf('-scee %.16e -scei %.16e -scie %.16e -scii %.16e',...
-            pm.scee, pm.scei, pm.scie, pm.scii);
+            pm.scee, pm.scie, pm.scei, pm.scii);
 st_neu_param =...
     sprintf('-n %d %d -mat %s -pr %.16e -ps %.16e %s',...
             pm.nE, pm.nI, mat_path, pm.pr, pm.ps, st_neu_s);
 st_sim_param =...
     sprintf('-t %.16e -dt %.17e -stv %.17e',...
             pm.t, pm.dt, pm.stv);
+if isfield(pm, 'seed') && (strcmpi(pm.seed, 'auto')==0 || isempty(pn.seed))
+    st_sim_param = [st_sim_param, sprintf(' -seed %d', pm.seed)];
+else
+    st_sim_param = [st_sim_param, ' --seed-auto-on'];
+end
 st_paths =...
     sprintf('--bin-save -o %s --save-spike-interval %s --save-spike %s',...
             output_name, output_ISI_name, output_RAS_name);
@@ -160,37 +192,48 @@ end
 
 if mode_read_only
     % fixme: is there a way to test whether the files are using by
-    % other program in Matlab? lsof is not portable to M$
+    % other program in Matlab? lsof is not portable to M$ Windows
+    %Note: dir().datenum only precise to seconds
+    %      stat().mtime can precise to nano seconds
     % Whether the file is exist and filled?
-    f_info = stat(output_RAS_name);
-    if ~isempty(f_info) && (time() - f_info.mtime > 1.0)
+    %f_info = stat(output_RAS_name);
+    %if ~isempty(f_info) && (time() - f_info.mtime > 1.0)
+    f_info = dir(output_RAS_name);
+    if ~isempty(f_info) && (datenum(clock()) - f_info.datenum > 1.0/86400)
         rt = 0;      % we have data
     else
         return
     end
 end
 
-% read and return data, if required
-if rt==0
-    if (nargout>0)
-        if return_X_name
-            X = output_name;
-        else
-            fid = fopen(output_name, 'r');
-            X = fread(fid, [p, Inf], 'double');
-            fclose(fid);
-        end
+if rt ~= 0
+    error('Fail to generate data!');
+end
+% At this point, the files should be generated, and ready for read.
+% Read and return data, if required
+if nargout > 0
+    % fixme: The Octave function isargout() is not in Matlab.
+    %        So seems no way to skip non-required data in Matlab
+    %        in some condition.
+    if return_X_name
+        X = output_name;
+    else
+        fid = fopen(output_name, 'r');
+        X = fread(fid, [p, Inf], 'double');
+        fclose(fid);
     end
     if (nargout>1)
         ISI = load('-ascii', output_ISI_name);
     end
     if (nargout>2)
-        % check if the file is non-empty
+        % Check if the file is non-empty (i.e. no any spike)
+        % Otherwise load() may fail due to empty file.
         tmp_fd = fopen(output_RAS_name);
         rt1 = fseek(tmp_fd, 0, 'eof');
         if rt1 ~= 0
             fclose(tmp_fd);
-            error('fseek?');
+            error(sprintf('fail to call fseek(), check the file "%s"',...
+                          output_RAS_name));
         end
         pos = ftell(tmp_fd);
         fclose(tmp_fd);
@@ -200,8 +243,6 @@ if rt==0
             ras = [];
         end
     end
-else
-    error('Fail to generate data!');
 end
 
 % delete data if asked
@@ -212,8 +253,7 @@ if mode_rm_only
     return
 end
 
-end
-
+%end
 %test
 %{
   pm.net  = 'net_2_2';
@@ -233,11 +273,13 @@ end
   pm.nI = 2;
   [X, ISI, ras] = gen_HH(pm);
 
-  [X] = gen_HH(pm, '');
-  [X] = gen_HH(pm, '');
-  [X] = gen_HH(pm, 'new');
-  [X] = gen_HH(pm, 'rm');
-  [X] = gen_HH(pm, 'new,rm');
+  pm = rmfield(pm, 'nI');
+  pm = rmfield(pm, 'nE');
+  X = gen_HH(pm, '');
+  X = gen_HH(pm, '');
+  X = gen_HH(pm, 'new');
+  X = gen_HH(pm, 'rm');  size(X)
+  X = gen_HH(pm, 'new,rm');  size(X)
   gen_HH(pm, '');
   gen_HH(pm, 'rm');
 
@@ -249,6 +291,6 @@ end
   X=gen_HH(pm,'nameX,rm');
 
   X=gen_HH(pm,'cmd');
-}%
+%}
 
 % vim: et sw=4 sts=4
