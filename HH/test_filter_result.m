a355ae36be623ac39877173a77d3256dbfd9eabe
xyy@xle <xyy@xle>
2014-05-22 02:15:49 +0800

    Tested spectrum based GC.

--- HH/test_filter_result.m
+++ HH/test_filter_result.m
@@ -1,4 +1,5 @@
 %
+% run analyse_GC_simple_HH.m first
 
 % show volt and srd samples
 rg_l = 100;
@@ -21,7 +22,7 @@ plot(1:rg_l, fscaling(srd(:,rg_b:rg_e)));
 X_l = filter(B',A',X')'; %conventional filtering
 %od_l = chooseOrderAuto(X_l, 'BIC', 100);
 od_l = 50;
-srd_l = WhiteningFilter(X_l, od_l, 'qr');
+srd_l = WhiteningFilter(X_l, od_l, 'qr'); % better result, but much slower
 %srd_l = WhiteningFilter(X_l, od_l);
 
 figure(3);
@@ -37,12 +38,31 @@ gc_X_l   = pos_nGrangerT2(X_l  , bic_od)
 gc_src_l = pos_nGrangerT2(srd_l, bic_od)
 
 fftlen = 1024;
-sX   = X2Sxx(X  , fftlen);
-sX_l = X2Sxx(X_l, fftlen);
+f_wnd = @(x) 0.5+0.5*cos(2*pi*x);
+sX   = mX2S_wnd( bsxfun(@minus, X  , mean(X  ,2)), [fftlen, 0.5], f_wnd);
+sX_l = mX2S_wnd( bsxfun(@minus, X_l, mean(X_l,2)), [fftlen, 0.5], f_wnd);
 %stv = 0.5;  % ms
 s_fq = (0:fftlen-1)/fftlen /stv*1000;  % Hz
 
-figure(7);
-plot(s_fq, sX, s_fq, sX_l);
+
+fS2dB = @(x) 10*log10(abs(x));
+
+figure(5);
+plot(s_fq, fS2dB(sX), s_fq, fS2dB(sX_l));
 xlim([0 1000]);
+
+use_od_sgcapp = 50;
+gc_x   = getGCSapp(sX  , use_od_sgcapp)
+gc_x_l = getGCSapp(sX_l, use_od_sgcapp)
+
+figure(6);
+sX_w = StdWhiteS(sX);
+plot(
+  s_fq, fS2dB(sX_w(:,1,1)),...
+  s_fq, fS2dB(sX_w(:,1,2)),...
+  s_fq, fS2dB(sX_l_w(:,1,2))...
+);
+xlim([0 1000]);
+ylim([-40, 10]);
+legend('sX_w(1,2)', 'sX_l_w(1,2)');
 
