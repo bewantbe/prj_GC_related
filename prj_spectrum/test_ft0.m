ec09441dd129e57dbf57054b0d7f11447c7c51a7
xyy@xhp7 <xyy@xhp7>
2014-05-22 00:09:43 +0800

    clean up.

--- prj_spectrum/test_ft0.m
+++ prj_spectrum/test_ft0.m
@@ -6,6 +6,10 @@ datalen = simu_time;
 
 st_data_source = 'hh';
 
+st_gen_cmd = 'new';
+st_gen_cmd = '';
+gc_scale = 1e4;
+
 tic
 switch st_data_source
   case 'ar'
@@ -15,7 +19,7 @@ switch st_data_source
     oX = gendata_linear(A, De, simu_time+len_extra);
     oX(:,1:len_extra) = [];
   case 'if'
-    oX = gendata_neu('net_2_2', 0.01, 1, 0.012, simu_time, 1.0, 'new -q -dt 0.125 --RC-filter');
+    oX = gendata_neu('net_2_2', 0.01, 1, 0.012, simu_time, 1.0, [st_gen_cmd ' -q -dt 0.125 --RC-filter']);
   case 'hh'
     pm.net  = 'net_2_2';
     pm.scee = 0.05;
@@ -23,7 +27,7 @@ switch st_data_source
     pm.pr   = 1.0;
     pm.t    = simu_time;
     pm.stv  = 1.0;
-    oX = gen_HH(pm, 'new');
+    oX = gen_HH(pm, st_gen_cmd);
   otherwise
     error('invalid value');
 end
@@ -34,31 +38,30 @@ oX = bsxfun(@minus, oX, mean(oX,2));
 use_od = chooseOrderAuto(oX);
 fprintf('oX(%d) -> pos_nGrangerT2(oX, od=%d) ',
         simu_time, use_od);
-gc = pos_nGrangerT2(oX, use_od); gc
+gc = pos_nGrangerT2(oX, use_od); gc_scale*gc
 
 use_od_sgcapp = 50;
 
-% Welch's spectrum estimation
-fftlen = round(sqrt(datalen)/2);
-[Su, fqsu] = mX2S_wnd(oX, [fftlen, 0.6], @(x)1-2*abs(x));
-fprintf('oX(%d) -> mX2S_wnd(fftlen=%d nslice=%d, wnd=1) -> getGCSapp(od=%d) ',
-        simu_time, fftlen, simu_time/fftlen, use_od_sgcapp);
-[gc, de11, de22] = getGCSapp(Su, use_od_sgcapp); gc
+% freq GC, Welch's spectrum estimation
+fftlen = round(sqrt(datalen));
+[Su, fqsu winfo] = mX2S_wnd(oX, [fftlen, 0.6], @(x)1-2*abs(x));
+fprintf('oX(%d) -> mX2S_wnd(mlen=%d nslice=%d, wnd) -> getGCSapp(od=%d) ',
+        simu_time, fftlen, winfo.n_slices, use_od_sgcapp);
+[gc, de11, de22] = getGCSapp(Su, use_od_sgcapp); gc_scale*gc
 
-% Multi-taper spectrum estimation
-HBW = 8;
-n_taper = floor(2*HBW);
-mlen = n_taper*round(sqrt(datalen));
-slen = mlen;
-[X1,T1] = SampleNonUnif(oX, mlen, slen, 'u');
-[S1 fqs] = mX2S_mt(X1, mlen, HBW, n_taper);
-S1 = S1*2*4;
-fqs = fqs/mlen;
-fprintf('oX(%d) -> u(mlen=%d nslice=%d) -> mX2S_mt(HBW=%g, ntaper=%d) -> getGCSapp(od=%d) ',
-        simu_time, mlen, simu_time/mlen, HBW, n_taper, use_od_sgcapp);
-[gc, de11, de22] = getGCSapp(S1, use_od_sgcapp); gc
-%figure(1);
-%plot(fftshift(fqs), fftshift(S1(:,1,1)), fftshift(fqsu), fftshift(Su(:,1,1)));
+% freq GC, Multi-taper spectrum estimation
+HBW = 4;
+n_taper = floor(2*HBW-1);
+mlen = 0.9*n_taper*round(sqrt(datalen));
+[S1_mt fqs_mt winfo] = mX2S_mt(oX, mlen, HBW, n_taper);
+fprintf('oX(%d) -> mX2S_mt(mlen=%d nslice=%d HBW=%g ntaper=%d) -> getGCSapp(od=%d) ',
+        simu_time, mlen, winfo.n_slices, HBW, n_taper, use_od_sgcapp);
+size(S1_mt)
+%[gc, de11, de22] = getGCSapp(S1_mt, use_od_sgcapp); gc_scale*gc
+
+figure(1);
+plot(fftshift(fqs_mt), fftshift(S1_mt(:,1,1)), fftshift(fqsu), fftshift(Su(:,1,1)));
+xlim([-0.5,0.5]);
 
 % time domain GC (whiten the data first)
 use_od_whiten = 20;
@@ -66,7 +69,7 @@ use_od = 50;
 srd = WhiteningFilter(oX, use_od);
 srd = bsxfun(@rdivide, srd, var(srd')');
 fprintf('oX(%d) -> Whitening(od=%d) -> normalize var -> nGrangerT(oX, od=%d) ',    simu_time, use_od_whiten, use_od);
-gc = nGrangerT(srd, use_od); gc
+gc = nGrangerT(srd, use_od); gc_scale*gc
 
 %S = A2S(A,De,1024);
 %S = permute(S,[3,1,2]);
