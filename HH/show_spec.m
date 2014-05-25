a56d3ef3d174eacdd63adf31e60d0c9bfc1d8735
xyy@xle <xyy@xle>
2014-05-25 19:23:28 +0800

    testing the non-invertable filter.

---
+++ HH/show_spec.m
@@ -0,0 +1,10 @@
+function show_spec(x1)
+fftlen = lowest_smooth_number_exact(sqrt(length(x1)));
+[sx, fqs] = mX2S_wnd(x1, [fftlen, 0.5], @(x)0.5+0.5*cos(2*pi*x));
+fqs = fftshift(fqs);
+sx  = fftshift(sx);
+%plot(fqs, sx);
+plot(fqs, 10*log10(abs(sx)));
+%semilogy(fqs, sx);
+xlim([0, 0.5]);
+ylabel('dB');
---
+++ HH/test_non_invertable.m
@@ -0,0 +1,51 @@
+%
+
+len = 1e6;
+x = randn(1, len);
+%figure(1);
+%show_spec(x);
+
+%x1 = diff(x);
+%x1 = filter([1 1], 1, x);
+%x1 = filter([1 2 1], 1, x);
+x1 = filter([1 3 3 1], 1, x);
+%x1 = filter([1 1.99 0.99], 1, x);
+
+%figure(2);
+%show_spec(x1);
+
+% ues MA
+%b = (-0.97).^(0:100);
+%b1_len = 100;
+%b = 1:-1/b1_len:0; b(end)=[];
+%b = b.*(-1).^(0:length(b)-1);
+%b = conv(b,b);
+%figure(10);
+%plot(b);
+%x2 = filter(b, 1, x1);
+%x2 = filter(b, 1, x2);
+
+% use AR
+%x2 = filter(1, [1 0.99], x1);
+%x2 = filter(1, [1 1.99 0.99], x1);
+%conv(conv([1 0.999],[1 0.999]),[1 0.999])
+%x2 = filter(1, [1   2.997   2.994   0.997], x1);
+x2 = filter(1, [1 2 1], x1);
+
+figure(2);
+bg = 1e5;
+rg = bg:bg+100-1;
+plot([x(rg); x2(rg)]');
+
+
+figure(3);
+show_spec(x2);
+
+use_od = 100;
+[xw, axw] = WhiteningFilter(x1,use_od);
+%figure(11);
+%plot(-axw);
+
+figure(12);
+show_spec(xw);
+
