09bf2eb107c25ad493bba885337a0d5202d0008f
xyy@xhp7 <xyy@xhp7>
2014-10-16 11:06:23 +0800

    1000, seems ISI is normal now.

--- sparse_net_gc/tmp_gen_data_test.m
+++ sparse_net_gc/tmp_gen_data_test.m
@@ -1,6 +1,6 @@
 % parameters to generate the network
 net_param.generator  = 'gen_sparse';
-net_param.p          = 100;
+net_param.p          = 1000;
 net_param.sparseness = 0.15;  % 0.30 0.20 0.15 0.10 0.05
 net_param.seed       = 123;
 net_param.software   = myif(exist('OCTAVE_VERSION','builtin'), 'octave', 'matlab');
@@ -10,7 +10,7 @@ clear('pm');
 pm.neuron_model = 'HH3_gcc';
 pm.net_param = net_param;
 pm.net  = gen_network(net_param);
-pm.nI   = net_param.p * 0.2;
+pm.nI   = net_param.p * 0.4;
 pm.scee = 0.05;
 pm.scie = 0.05;
 pm.scei = 0.09;
@@ -20,6 +20,6 @@ pm.ps   = 0.03;
 pm.t    = 1e3;
 pm.stv  = 0.5;
 
-[X2, ISI2, ras2] = gen_HH(pm, 'rm');
-
+[X2, ISI2, ras2] = gen_HH(pm, 'rm');  % ,ext_T
+var(ISI2)
 
