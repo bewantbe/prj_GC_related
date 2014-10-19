89169f838249919e59c827f1c1b143959f03fce3
xyy@xhp7 <xyy@xhp7>
2014-10-20 02:37:27 +0800

    test case fix.

--- sparse_net_gc/tmp_gen_data_test.m
+++ sparse_net_gc/tmp_gen_data_test.m
@@ -1,22 +1,22 @@
 % parameters to generate the network
 net_param.generator  = 'gen_sparse';
 net_param.p          = 1000;
-net_param.sparseness = 0.15;  % 0.30 0.20 0.15 0.10 0.05
+net_param.sparseness = 0.025;  % 0.30 0.20 0.15 0.10 0.05
 net_param.seed       = 123;
 net_param.software   = myif(exist('OCTAVE_VERSION','builtin'), 'octave', 'matlab');
 gen_network = @(np) eval(sprintf('%s(np);', np.generator));
 
 clear('pm');
-pm.neuron_model = 'HH3_gcc';
+pm.neuron_model = 'HH3_gcc49_s';
 pm.net_param = net_param;
 pm.net  = gen_network(net_param);
-pm.nI   = net_param.p * 0.25;
+pm.nI   = floor(net_param.p * 0.25);
 pm.scee = 0.04;
 pm.scie = 0.04;
 pm.scei = 0.07;
 pm.scii = 0.07;
-pm.pr   = 0.5;
-pm.ps   = 0.03;
+pm.pr   = 0.7;
+pm.ps   = 0.032;  % 0.032 = 1 mV
 pm.t    = 1e3;
 pm.stv  = 0.5;
 
