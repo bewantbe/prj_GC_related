3453be74b7d9dccebfce4bd7c5673cdce97cc4fa
xyy@xle <xyy@xle>
2014-11-13 04:49:51 +0800

    polished nGrangerTfast.m

--- sparse_net_gc/nGrangerTfast.m
+++ sparse_net_gc/nGrangerTfast.m
@@ -1,39 +1,44 @@
 %
 
-function GC = nGrangerTfast(X, m)
-
-  if exist('wcov', 'var')
-    % whiten the covariance (reduce condition number)
-    %[A2d, D] = ARregressionpd(covz);
-    %fftlen = max(1024, 5*m);
-    %S = A2S(A2d, D, fftlen);
-    %S = StdWhiteS(S);
-    %R = S2cov(S, m);
-    %covz = getcovzpd(X, m);
-    %[A2d, D] = ARregressionpd(covz);
+function GC = nGrangerTfast(X, m, b_whiten_first)
+  p = size(X, 1);
+  if exist('b_whiten_first', 'var') && b_whiten_first ~= 0
+    % whiten data can help reduce condition number
+    switch b_whiten_first
+    case 1
+      % whiten time series data directly
+      X = WhiteningFilter(X, m);  % whiten data
+      covz = getcovzpd(X, m);
+      [A2d, D] = ARregressionpd(covz, p);
+    case 2
+      % whiten the covariance in frequency domain
+      fftlen = max(1024, 5*m);
+      covz = getcovzpd(X, m);
+      [A2d, D] = ARregressionpd(covz, p);
+      S = A2S(A2d, D, fftlen);
+      S = StdWhiteS(S);
+      R = S2cov(S, m);
+      covz = R2covz(R);
+      [A2d, D] = ARregressionpd(covz, p);
+    otherwise
+      error('no this mode');
+    end
   else
-    % whiten original data
-    p = size(X, 1);
-    %X = WhiteningFilter(X, m);  % whiten data, reduce condition number
-    %covz = getcovzpd(X, m);
-    %[A2d, D] = ARregressionpd(covz, p);
     R = getcovpd(X, m);
     covz = R2covz(R);
     [A2d, D] = ARregression(R);
+    %covz = getcovzpd(X, m);
+    %[A2d, D] = ARregressionpd(covz, p);
   end
-
-  a = reshape(-A2d, p,p,[]);
-  a = permute(a, [3,1,2]);   % index: time lag, i, j  (j->i)
 
   d = diag(D);
   Qz = inv(covz(p+1:end, p+1:end));
-  id_0 = 0:p:p*m-1;
-  %disp('-- begin RGrangerT --');
-  %RGrangerT(R)
-  %disp('-- end RGrangerT --');
 
-  covz_orig = covz;
-  permvec0 = 1 : p;
+  a = reshape(-A2d, p,p,[]);
+  a = permute(a, [3,1,2]);   % index: (time lag, i, j)  (j->i)
+
+  id_0 = 0:p:p*m-1;
+
   GC = zeros(p, p);
   for j = 1 : p
     Qjj = Qz(id_0+j, id_0+j);
