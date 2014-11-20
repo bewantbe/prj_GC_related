84ac8b6c64698a82f36ed7b9a715a79c122db0b3
xyy@xle <xyy@xle>
2014-11-21 06:50:45 +0800

    Make the code RGrangerTLevinson.m looks better.

--- sparse_net_gc/RGrangerTLevinson.m
+++ sparse_net_gc/RGrangerTLevinson.m
@@ -1,23 +1,19 @@
 % Calculate GC without explicitly do p-1 variable regressions
-% Much faster than RGrangerT(R) for large variable case.
+% Do regression by Levinson method (Toeplitz)
+% Much faster than RGrangerT(R) for large variables case.
+% Significantly faster than RGrangerTfast(R) for large variables case
 
 function GC = RGrangerTLevinson(R)
-  % so R is 2d cov series
-  p = size(R, 1);
-  m = size(R, 2) / p - 1;
-  %covz = R2covz(R);
-  %[A2d, D] = ARregressionpd(covz, p);
-  %Qz = inv(covz(p+1:end, p+1:end));
-  [s_Qjj, A2d, D] = GetQyy(R);
+  [s_Qjj, A, D] = GetQyy(R);
 
-  a = reshape(-A2d, p,p,[]);
-  a = permute(a, [3,1,2]);   % index: (time_lag, i, j)  (j->i)
+  p = size(R, 1);
+  A = reshape(-A, p,p,[]);  % index: (i, j, time_lag)
+  A = permute(A, [3,1,2]);  % index: (time_lag, i, j)  (j->i)
   d = diag(D);
-  id_0 = 0:p:p*m-1;
 
   GC = zeros(p, p);
   for j = 1 : p
-    GC(:, j) = log1p(sum(s_Qjj(:,:,j) \ a(:,:,j) .* a(:,:,j))' ./ d);
+    GC(:, j) = log1p(sum(s_Qjj(:,:,j) \ A(:,:,j) .* A(:,:,j))' ./ d);
     GC(j, j) = 0;
   end
 end
