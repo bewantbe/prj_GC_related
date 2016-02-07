1b0f4edfd4836e5a4396e2ad05e5fd4f3a05f8b3
xyy@xle <xyy82148@sjtu.edu.cn>
2016-02-08 01:01:45 +0800

    simple update.

--- HH/scan_worker/scan_scee_worker_w3.m
+++ HH/scan_worker/scan_scee_worker_w3.m
@@ -3,6 +3,6 @@ neuron_model = 'HH3_gcc49_westmere2';
 pr = 5.93;
 ps = 0.032;
 s_scee = 0.05*linspace(0, 1, 30);
-simu_time = 5e7;  % expect: 1.9 hour, actual: 0.944 hour
+simu_time = 5e7;  % expect: 1.9 hour, actual: 1.03 hour
 
 scan_scee_worker_head;
