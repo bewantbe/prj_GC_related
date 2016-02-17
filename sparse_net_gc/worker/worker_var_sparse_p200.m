8709aa397e463597e30ee19687fc61da9e17cad5
xyy@xle <xyy82148@sjtu.edu.cn>
2016-02-17 22:58:23 +0400

    Using prob sampling in sparseness.

--- sparse_net_gc/worker/worker_var_sparse_p200.m
+++ sparse_net_gc/worker/worker_var_sparse_p200.m
@@ -3,7 +3,8 @@
 simu_time = 1e6;
 p = 200;
 n_indirect = 10;
-s_sparseness = linspace(1, round(sqrt(p*n_indirect)), 30)/p;
+%s_sparseness = linspace(1, round(sqrt(p*n_indirect)), 30)/p;
+s_sparseness = sort(ceil(round(sqrt(p*n_indirect))*rand(1, 33)))/p;
 ncpu = 11;
 prefix_tmpdata = ['data/' mfilename '_' datestr(now, 30) '_'];
 worker_head_sparse_test;
