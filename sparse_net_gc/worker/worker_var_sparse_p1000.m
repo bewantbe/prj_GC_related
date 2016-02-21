2213a3d9e766dcc477dacf2d6a181d579270c788
xyy@xle <xyy82148@sjtu.edu.cn>
2016-02-21 07:08:33 +0400

    Update worker.

--- sparse_net_gc/worker/worker_var_sparse_p1000.m
+++ sparse_net_gc/worker/worker_var_sparse_p1000.m
@@ -6,6 +6,5 @@ n_indirect = 10;
 %s_sparseness = linspace(1, round(sqrt(p*n_indirect)), 11)/p;
 s_sparseness = sort(ceil(round(sqrt(p*n_indirect))*rand(1, 11)))/p;
 ncpu = 11;
-prefix_tmpdata = ['data/' mfilename '_' datestr(now, 30) '_'];
 worker_head_sparse_test;
 
