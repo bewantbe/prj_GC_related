e3956bff782fe239052affacdb0fd07a6c4f477d
xyy@xle <xyy82148@sjtu.edu.cn>
2016-02-25 17:33:30 +0400

    Split show and show core for big net scan.

--- sparse_net_gc/worker/worker_show_sparse.m
+++ sparse_net_gc/worker/worker_show_sparse.m
@@ -1,16 +1,3 @@
-% show result of parallel_main
-    fontsize = 19;
-    linewidth = 1;
-    pic_prefix = 'pic_tmp/';
-    pic_output       = @(st)print('-deps',  [pic_prefix, st, '.eps']);
-    pic_output_color = @(st)print('-depsc2',[pic_prefix, st, '.eps']);
-%    set(0, 'defaultfigurevisible', 'off');
-    set(0, 'defaultlinelinewidth', linewidth);
-    set(0, 'defaultaxesfontsize', fontsize);
-
-%set(0, 'defaultfigurevisible', 'off');
-addpath([getenv('HOME') '/matcode/GC_clean/GCcal/']);
-
 % load results
 s_data_file_name = {
 %'scan_HH3_gcc49_westmere2_sparse=1.0e-02-2.0e-01_p=100_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+05'
@@ -23,121 +10,30 @@ s_data_file_name = {
 %'scan_HH3_gcc49_westmere2_nogui_sparse=1.3e-02-2.2e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=2.0e+04_2199015[3]'
 %'scan_HH3_gcc49_westmere2_nogui_sparse=1.8e-02-2.2e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=2.0e+04_2199015[4]'
 %}
+%{
+%'scan_HH3_gcc49_westmere2_nogui_sparse=2.5e-03-2.1e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[1]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=7.6e-03-2.1e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[2]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=1.3e-02-2.2e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[3]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=1.8e-02-2.2e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[4]'
+%}
 {
-'scan_HH3_gcc49_westmere2_nogui_sparse=2.5e-03-2.1e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[1]'
-'scan_HH3_gcc49_westmere2_nogui_sparse=7.6e-03-2.1e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[2]'
-'scan_HH3_gcc49_westmere2_nogui_sparse=1.3e-02-2.2e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[3]'
-'scan_HH3_gcc49_westmere2_nogui_sparse=1.8e-02-2.2e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199052[4]'
+'scan_HH3_gcc49_westmere2_nogui_sparse=2.5e-03-1.5e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199482[1]'
+'scan_HH3_gcc49_westmere2_nogui_sparse=6.1e-03-1.5e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199482[2]'
+'scan_HH3_gcc49_westmere2_nogui_sparse=9.7e-03-1.5e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199482[3]'
+'scan_HH3_gcc49_westmere2_nogui_sparse=1.3e-02-1.6e-01_p=400_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199482[4]'
 }
+%{
+%'scan_HH3_gcc49_westmere2_nogui_sparse=1.5e-02-2.2e-01_p=200_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199483[3]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=2.0e-02-2.3e-01_p=200_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199483[4]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=1.0e-02-2.1e-01_p=200_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199483[2]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=5.0e-03-2.1e-01_p=200_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199483[1]'
+%}
+%{
+%'scan_HH3_gcc49_westmere2_nogui_sparse=3.2e-02-3.2e-01_p=100_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199484[4]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=2.4e-02-3.1e-01_p=100_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199484[3]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=1.7e-02-3.1e-01_p=100_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199484[2]'
+%'scan_HH3_gcc49_westmere2_nogui_sparse=1.0e-02-3.0e-01_p=100_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199484[1]'
+%}
 };
 
-gen_network = @(np) eval(sprintf('%s(np);', np.generator));
-
-for id_s_data = 1:length(s_data_file_name)
-
-  if iscell(s_data_file_name{id_s_data})
-    cell_data_file = s_data_file_name{id_s_data};
-    c_data = {};
-    c_jobs = {};
-    for id_c_data = 1:numel(cell_data_file)
-      data_file_name = ['scan_sparseness/' cell_data_file{id_c_data} '.mat'];
-      load(data_file_name);  % load 's_data', 's_jobs', 'in_const_data'
-      c_data = {c_data{:}, s_data{:}};
-      c_jobs = {c_jobs{:}, s_jobs{:}};
-      size(c_data)
-    end
-    s_data = c_data;
-    s_jobs = c_jobs;
-  else
-    data_file_name = ['scan_sparseness/' s_data_file_name{id_s_data} '.mat'];
-    load(data_file_name);  % load 's_data', 's_jobs', 'in_const_data'
-  end
-  
-  pm        = in_const_data.pm;
-  net_param = in_const_data.net_param;
-  use_od    = in_const_data.use_od;
-  len = round(pm.t / pm.stv);
-  p = pm.nE+pm.nI;
-
-  % variables in plot
-  s_sparseness = zeros(size(s_jobs));
-  s_sparseness_true = s_sparseness;
-  s_correct_rate_best_guess = zeros(size(s_jobs));
-  s_correct_rate_pval = zeros(size(s_jobs));
-  s_rate = zeros(size(s_jobs));
-  s_rate_std = zeros(size(s_jobs));
-  for id_job=1:numel(s_jobs)
-    in = s_jobs{id_job};
-    ou = s_data{id_job};
-    s_sparseness(id_job) = in.sparseness;
-    
-    % get the answer
-    net_param.sparseness = in.sparseness;
-    net_param.seed       = ou.net_seed;
-    neu_network = gen_network(net_param);
-
-    GC = ou.pairGC;
-
-    % flatten the GC and connection matrix
-    plain_gc = GC(eye(p)==0);
-    plain_network = neu_network(eye(p)==0);
-
-    % auto determine cutline
-    [sorted_gc, sorted_gc_id] = sort(plain_gc);
-    sorted_gc_zeroone = plain_network(sorted_gc_id);
-    tmp_0 = cumsum(sorted_gc_zeroone==0);
-    tmp_0 = tmp_0(end) - tmp_0;
-    tmp_1 = cumsum(sorted_gc_zeroone>0);
-    [~, min_err_id] = min([tmp_0; 0] + [0; tmp_1]);
-    min_err_gc = sorted_gc(min_err_id);
-    p_val_min_err = 1-chi2cdf(min_err_gc*len, use_od);
-    neu_network_best_guess = GC >= min_err_gc;
-    adj_cmp_best_guess = neu_network_best_guess - neu_network;
-
-    s_correct_rate_best_guess(id_job) = sum(0==adj_cmp_best_guess(eye(p)==0))/(p*(p-1));
-
-    % cutline according to p-value
-    p_val = min(0.01, 1 / p);
-    gc_zero_line = chi2inv(1-p_val, use_od)/len;
-    neu_network_pval = GC >= gc_zero_line;
-    adj_cmp_pval = neu_network_pval - neu_network;
-
-    s_correct_rate_pval(id_job) = sum(0==adj_cmp_pval(eye(p)==0))/(p*(p-1));
-
-    s_sparseness_true(id_job) = sum(plain_network)/(p*(p-1));
-
-    s_rate(id_job) = mean(1000 ./ ou.ISI);
-    s_rate_std(id_job) = std(1000 ./ ou.ISI);
-  end
-
-  [s_sparseness, id_sort] = sort(s_sparseness);
-  s_correct_rate_best_guess = s_correct_rate_best_guess(id_sort);
-  s_correct_rate_pval = s_correct_rate_pval(id_sort);
-  s_rate = s_rate(id_sort);
-  s_rate_std = s_rate_std(id_sort);
-
-  figure(1);
-  plot(s_sparseness, 100*s_correct_rate_pval, 'o');
-  xlabel('sparseness');
-  ylabel('p-val correct reconstruction ratio (pairwise GC)');
-  set(gca, 'xdir', 'reverse');
-%  ylim([50 100]);
-  pic_output_color(sprintf('scan_sparse_correct_pval_p=%d+%d_pr=%.1e_ps=%.1e_scee=%.1e_t=%.1e', pm.nE, pm.nI, pm.pr, pm.ps, pm.scee, pm.t));
-
-  figure(2);
-  plot(s_sparseness, 100*s_correct_rate_best_guess, 'o');
-  xlabel('sparseness');
-  ylabel('best edge correct reconstruction ratio (pairwise GC)');
-  set(gca, 'xdir', 'reverse');
-  ylim([50 100]);
-  pic_output_color(sprintf('scan_sparse_correct_best_p=%d+%d_pr=%.1e_ps=%.1e_scee=%.1e_t=%.1e', pm.nE, pm.nI, pm.pr, pm.ps, pm.scee, pm.t));
-
-  figure(3);
-  errorbar(s_sparseness, s_rate, s_rate_std);
-  xlabel('sparseness');
-  ylabel('firing rate (distribution) (Hz)');
-  set(gca, 'xdir', 'reverse');
-  pic_output_color(sprintf('scan_sparse_ISI_p=%d+%d_pr=%.1e_ps=%.1e_scee=%.1e_t=%.1e', pm.nE, pm.nI, pm.pr, pm.ps, pm.scee, pm.t));
-
-end
-
+worker_show_sparse_core;
---
+++ sparse_net_gc/worker/worker_show_sparse_core.m
@@ -0,0 +1,133 @@
+% show result of parallel_main
+    fontsize = 19;
+    linewidth = 1;
+    pic_prefix = 'pic_tmp/';
+    pic_output       = @(st)print('-deps',  [pic_prefix, st, '.eps']);
+    pic_output_color = @(st)print('-depsc2',[pic_prefix, st, '.eps']);
+%    set(0, 'defaultfigurevisible', 'off');
+    set(0, 'defaultlinelinewidth', linewidth);
+    set(0, 'defaultaxesfontsize', fontsize);
+
+%set(0, 'defaultfigurevisible', 'off');
+addpath([getenv('HOME') '/matcode/GC_clean/GCcal/']);
+
+gen_network = @(np) eval(sprintf('%s(np);', np.generator));
+
+for id_s_data = 1:length(s_data_file_name)
+
+  if iscell(s_data_file_name{id_s_data})
+    cell_data_file = s_data_file_name{id_s_data};
+    c_data = {};
+    c_jobs = {};
+    for id_c_data = 1:numel(cell_data_file)
+      data_file_name = ['scan_sparseness/' cell_data_file{id_c_data} '.mat'];
+      load(data_file_name);  % load 's_data', 's_jobs', 'in_const_data'
+      c_data = {c_data{:}, s_data{:}};
+      c_jobs = {c_jobs{:}, s_jobs{:}};
+      size(c_data)
+    end
+    s_data = c_data;
+    s_jobs = c_jobs;
+  else
+    data_file_name = ['scan_sparseness/' s_data_file_name{id_s_data} '.mat'];
+    load(data_file_name);  % load 's_data', 's_jobs', 'in_const_data'
+  end
+  
+  pm        = in_const_data.pm;
+  net_param = in_const_data.net_param;
+  use_od    = in_const_data.use_od;
+  len = round(pm.t / pm.stv);
+  p = pm.nE+pm.nI;
+
+  % variables in plot
+  s_sparseness = zeros(size(s_jobs));
+  s_sparseness_true = s_sparseness;
+  s_correct_rate_best_guess = zeros(size(s_jobs));
+  s_correct_rate_pval = zeros(size(s_jobs));
+  s_rate = zeros(size(s_jobs));
+  s_rate_std = zeros(size(s_jobs));
+  for id_job=1:numel(s_jobs)
+    in = s_jobs{id_job};
+    ou = s_data{id_job};
+    s_sparseness(id_job) = in.sparseness;
+    
+    % get the answer
+    net_param.sparseness = in.sparseness;
+    net_param.seed       = ou.net_seed;
+    neu_network = gen_network(net_param);
+
+    GC = ou.pairGC;
+
+    % flatten the GC and connection matrix
+    plain_gc = GC(eye(p)==0);
+    plain_network = neu_network(eye(p)==0);
+
+    % auto determine cutline
+    [sorted_gc, sorted_gc_id] = sort(plain_gc);
+    sorted_gc_zeroone = plain_network(sorted_gc_id);
+    tmp_0 = cumsum(sorted_gc_zeroone==0);
+    tmp_0 = tmp_0(end) - tmp_0;
+    tmp_1 = cumsum(sorted_gc_zeroone>0);
+    [~, min_err_id] = min([tmp_0; 0] + [0; tmp_1]);
+    min_err_gc = sorted_gc(min_err_id);
+    p_val_min_err = 1-chi2cdf(min_err_gc*len, use_od);
+    neu_network_best_guess = GC >= min_err_gc;
+    adj_cmp_best_guess = neu_network_best_guess - neu_network;
+
+    s_correct_rate_best_guess(id_job) = sum(0==adj_cmp_best_guess(eye(p)==0))/(p*(p-1));
+
+    % cutline according to p-value
+    p_val = min(0.01, 1 / p);
+    gc_zero_line = chi2inv(1-p_val, use_od)/len;
+    neu_network_pval = GC >= gc_zero_line;
+    adj_cmp_pval = neu_network_pval - neu_network;
+
+    s_correct_rate_pval(id_job) = sum(0==adj_cmp_pval(eye(p)==0))/(p*(p-1));
+
+    s_sparseness_true(id_job) = sum(plain_network)/(p*(p-1));
+
+    s_rate(id_job) = mean(1000 ./ ou.ISI);
+    s_rate_std(id_job) = std(1000 ./ ou.ISI);
+  end
+
+  [s_sparseness, id_sort] = sort(s_sparseness);
+  s_correct_rate_best_guess = s_correct_rate_best_guess(id_sort);
+  s_correct_rate_pval = s_correct_rate_pval(id_sort);
+  s_rate = s_rate(id_sort);
+  s_rate_std = s_rate_std(id_sort);
+  
+  param_str = sprintf('p=%d+%d_pr=%.1e_ps=%.1e_scee=%.1e_t=%.1e', pm.nE, pm.nI, pm.pr, pm.ps, pm.scee, pm.t);
+  if isfield(in_const_data, 'identity_str')
+    if isempty(strfind(in_const_data.identity_str, '['))
+      param_str = [param_str '_j' in_const_data.identity_str];
+    else
+      tmp_p = strfind(in_const_data.identity_str, '[')-1;
+      param_str = [param_str '_j' in_const_data.identity_str(1:tmp_p)];
+    end
+  end
+
+  figure(1);
+  plot(s_sparseness, 100*s_correct_rate_pval, 'o');
+  xlabel('sparseness');
+  ylabel('p-val correct reconstruction ratio (pairwise GC)');
+  set(gca, 'xdir', 'reverse');
+%  ylim([50 100]);
+  pic_output_color(['scan_sparse_correct_pval_' param_str]);
+
+  figure(2);
+  plot(s_sparseness, 100*s_correct_rate_best_guess, 'o');
+  xlabel('sparseness');
+  ylabel('best edge correct reconstruction ratio (pairwise GC)');
+  set(gca, 'xdir', 'reverse');
+  ylim([50 100]);
+  pic_output_color(['scan_sparse_correct_best_' param_str]);
+
+  figure(3);
+  errorbar(s_sparseness, s_rate, s_rate_std);
+  xlabel('sparseness');
+  ylabel('firing rate (distribution) (Hz)');
+  set(gca, 'xdir', 'reverse');
+  pic_output_color(['scan_sparse_ISI_' param_str]);
+
+end
+
