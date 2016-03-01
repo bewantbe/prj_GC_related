867c0e4765180ef1afc511cbf2b6af7eedc68b45
xyy@xle <xyy82148@sjtu.edu.cn>
2016-03-02 02:32:45 +0400

    work stage.

--- sparse_net_gc/approx/ana_gc_app.m
+++ sparse_net_gc/approx/ana_gc_app.m
@@ -1,5 +1,8 @@
 %
 
+if ~exist('ed', 'var') || isempty(ed) || ~ed
+ed = true;
+
 %gen_data_n10_c1;
 gen_data_n40_c1;
 %gen_data_n100_c1;
@@ -45,16 +48,20 @@ tic;
 [pairGC, ~, pairA] = pairRGrangerT(R);
 toc;
 
-%return
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 id1 = 1;
 id2 = 2;
 
+gc_ans_joint = GC(id1, id2)
+gc_ans_pair = pairGC(id1, id2)
+
 id3 = 1:p;
 id3([id1 id2]) = [];
 
 fftlen = size(S,3);
+m = use_od;
 
 Qw = zeros(size(S));
 for k = 1:fftlen
@@ -72,47 +79,48 @@ HTR = @(x) permute(conj(x), [2, 1, 3:ndims(x)]);
 A1c_w = permute(reshape(A(id1, :), p, []), [3 1 2]);
 A12_w = fft(A1c_w(1,id2,:), fftlen, 3);
 A13_w = fft(A1c_w(1,id3,:), fftlen, 3);
-ft_b12_app = A12_w - rdiv3d( A13_w, Qzz_w, Qzy_w);
-ift_b12_app = real( ifft(ft_b12_app, fftlen) );
-ift_b12_app(floor((end+1)/2)+1:end) = 0;
-ft_b12_app = fft(ift_b12_app);
+b12_w_app = A12_w - rdiv3d( A13_w, Qzz_w, Qzy_w);  % key
+b12_t_app = real( ifft(b12_w_app, fftlen, 3) );
+b12_t_app(floor((end+1)/2)+1:end) = 0;
+b12_w_app = fft(b12_t_app, fftlen, 3);
 
-%ft_b12_app = squeeze(ft_b12_app)';
-%gc_ift_b12_app_full = sum(ft_b12_app .* conj(ft_b12_app))
-%gc_ift_b12_app_full = sum(ft_b12_app ./ (Qyy_w - rdiv3d(Qyz_w, Qzz_w, Qzy_w)) .* conj(ft_b12_app)) / R(id2,id2)
+gc_b12_w_app = mean(b12_w_app .* conj(b12_w_app))
+gc_b12_t_app_full = mean(b12_w_app ./ (Qyy_w - rdiv3d(Qyz_w, Qzz_w, Qzy_w)) .* conj(b12_w_app)) / R(id2,id2)
 
-gc_ift_b12_app = ift_b12_app(1:m) * ift_b12_app(1:m)'
+gc_b12_t_app = sum(b12_t_app(1:m).^2)
 
 %Qzz_w_app = S(id3,id3,:) - rdiv3d(S(id3,[id1 id2],:), S([id1 id2],[id1 id2],:), S([id1 id2], id3,:)); % - rdiv3d(eye(p-2),Qw(id3,id3,:))
 Qzz_w_app = ...
-  rdiv3d( HTR(A3d_w(id1,id3,:)), De(1,1), A3d_w(1,3:end,:)) + ...
-  rdiv3d( HTR(A3d_w(id2,id3,:)), De(2,2), A3d_w(2,3:end,:)) + ...
-  rdiv3d( HTR(A3d_w(3:end,3:end,:)), De(3:end,3:end), A3d_w(3:end,3:end,:));
+  rdiv3d( HTR(A3d_w(id1,id3,:)), De(id1,id1), A3d_w(id1,id3,:)) + ...
+  rdiv3d( HTR(A3d_w(id2,id3,:)), De(id2,id2), A3d_w(id2,id3,:)) + ...
+  rdiv3d( HTR(A3d_w(id3,id3,:)), De(id3,id3), A3d_w(id3,id3,:));
 
 %Qzz_w_app = ...
 %  rdiv3d( HTR(A3d_w(3:end,3:end,:)), De(3:end,3:end), A3d_w(3:end,3:end,:));
 
-ft_b12_app_v4 = A12_w - rdiv3d( A13_w, Qzz_w_app, Qzy_w);
-ift_b12_app_v4 = real( ifft(ft_b12_app_v4, fftlen) );
-ift_b12_app_v4 = squeeze(ift_b12_app_v4(1:m))';
-gc_ift_b12_app_v4 = ift_b12_app_v4 * ift_b12_app_v4'
+b12_w_app_v4 = A12_w - rdiv3d( A13_w, Qzz_w_app, Qzy_w);  % key
+b12_t_app_v4 = real( ifft(b12_w_app_v4, fftlen, 3) );
+b12_t_app_v4(floor((end+1)/2)+1:end) = 0;
+b12_w_app_v4 = fft(b12_t_app_v4, fftlen, 3);
+
+gc_b12_t_app_v4 = sum(b12_t_app_v4(1:m).^2)
 
 figure(3);
-plot(1:m, pairwA(id1,id2:p:end), '-x',...
-     1:m, squeeze(ift_b12_app(1:m)), '-+',...
-     1:m, ift_b12_app_v4(1:m), '-o',...
-     1:m, wA(id1,id2:p:end), '-.');
+plot(1:m, pairA(id1,id2:p:end), '-x',...
+     1:m, squeeze(b12_t_app(1:m)), '-+',...
+     1:m, squeeze(b12_t_app_v4(1:m)), '-o',...
+     1:m, A(id1,id2:p:end), '-.');
 legend('cal', 'app full', 'app acoef', 'cond')
 print('-depsc2', 'e.eps');
 
-ft_b12_app_v4 = - rdiv3d( A13_w, Qzz_w_app, Qzy_w);
-ift_b12_app_v4 = real( ifft(ft_b12_app_v4, fftlen) );
-ift_b12_app_v4 = squeeze(ift_b12_app_v4(1:2*m))';
+diff_pairA_A = pairA(id1,id2:p:end) - A(id1,id2:p:end);
+diff_b12_t_app_A = squeeze(b12_t_app(1:m))' - A(id1,id2:p:end);
+diff_b12_t_app_v4_A = squeeze(b12_t_app_v4(1:m))' - A(id1,id2:p:end);
 
 figure(4);
-plot(1:m, pairwA(id1,id2:p:end) - wA(id1,id2:p:end), '-x',...
-     1:m, squeeze(ift_b12_app(1:m))' - wA(id1,id2:p:end), '-+',...
-     1:2*m, ift_b12_app_v4(1:2*m), '-o')
-
+plot(1:m, diff_pairA_A, '-x',...
+     1:m, diff_b12_t_app_A, '-+',...
+     1:m, diff_b12_t_app_v4_A, '-o')
+legend('pairA - A', 'app full - A', 'app Q - A')
 
 % vim: set ts=2 sw=2 ss=2
