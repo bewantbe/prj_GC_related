aed2a3de41269ef0a5f1a5196fdbf996b45651ea
xyy@xle <xyy82148@sjtu.edu.cn>
2016-03-17 03:30:54 +0400

    update.

--- sparse_net_gc/approx/inv_test.m
+++ sparse_net_gc/approx/inv_test.m
@@ -1,5 +1,5 @@
 %
-if 1
+if 0
 a = [0.5 0.2 0.1];
 
 od = 6;
@@ -20,14 +20,19 @@ return
 end
 
 %% matrix case
+if 0
 
 a = [
  0.5 0.1 0.21 0.15
 -0.2 0.4 0.33 0.09];
+a = [a zeros(2, 4)]
 
 %a = [0.5 0.2 0.1];
 
-od = 3;
+od = 6;
+
+p = size(a,1);
+A3d = cat(3, eye(p), reshape(a,p,p,[]), zeros(p,p, od-size(a,2)/p));
 
 fftlen = 1024;
 S = A2S(a, eye(size(a,1)), fftlen);
@@ -38,8 +43,6 @@ Q(abs(Q)<1e-14)=0;
 
 %tA = toeplitz([1; zeros(od,1)], [1 a zeros(1, od-size(a,2))]);
 
-p = size(a,1);
-A3d = cat(3, eye(p), reshape(a,p,p,[]), zeros(p,p, od-size(a,2)/p));
 tA = zeros((od+1)*p);
 tB = zeros((od+1)*p);
 for k=0:od
@@ -52,10 +55,19 @@ tQ = tA' * tA;
 tQ - Q
 maxerr = @(x) max(abs(x(:)));
 
+return
+end
 
+%%%%%%%%%%%%%%%%%%%%%%
+% Qyy
 
-%Q_g = ...
-%[Id a       O
-%a' Id+a'*a  a
-%O    a'     Id]
+id_y = 2;
+id_comb = p*(0:od);
+
+tAy = zeros((od+1)*p, od+1);
+for k=0:od
+  tAy(k*p+1:(k+1)*p, k+1:end) = reshape(A3d(:,id_y,1:end-k), p, []);
+end
+
+tAy' * tAy - Q(id_y+id_comb, id_y+id_comb)
 
