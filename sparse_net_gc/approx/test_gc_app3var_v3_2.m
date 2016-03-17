49f0c28be784c6f5796ecaa4227aa3678382c531
xyy@xle <xyy82148@sjtu.edu.cn>
2016-03-17 22:09:57 +0400

    Move data preparation together.

--- sparse_net_gc/approx/test_gc_app3var_v3_2.m
+++ sparse_net_gc/approx/test_gc_app3var_v3_2.m
@@ -10,34 +10,64 @@ m = use_od;
 ed = 0;
 tic;
 if ~exist('ed', 'var') || isempty(ed) || ~ed
-ed = true;
+  ed = true;
 
-gen_data_n10_c1;
-%gen_data_n40_c1;  % 1 40
-%gen_data_n100_c1;  % 32  32
+  gen_data_n10_c1;
+  %gen_data_n40_c1;  % 1 40
+  %gen_data_n100_c1;  % 32  32
 
-len = size(X,2);
+  len = size(X,2);
 
-    % whiten the covariance in frequency domain
-    covz = getcovzpd(X, fit_od);
-    %[A2d, De] = ARregressionpd(covz, p);
+  % whiten the covariance in frequency domain
+  covz = getcovzpd(X, fit_od);
+  %[A2d, De] = ARregressionpd(covz, p);
 
-    % purify coef
-    [GC De A2d] = RGrangerTfast(covz, p);
-    id_no_conn = GC < 2*fit_od/len;
-    id_no_conn(eye(p)==1) = 0;
-    id_a_zero = find(id_no_conn);
-    A2d(bsxfun(@plus, id_a_zero, 0:p*p:end-1)) = 0;
-    De(eye(p)==0) = 0;
+  % purify coef
+  [GC De A2d] = RGrangerTfast(covz, p);
+  %id_no_conn = GC < 2*fit_od/len;
+  %id_no_conn(eye(p)==1) = 0;
+  %id_a_zero = find(id_no_conn);
+  %A2d(bsxfun(@plus, id_a_zero, 0:p*p:end-1)) = 0;
+  %De(eye(p)==0) = 0;
 
-    S = A2S(A2d, De, max(pow2ceil(8*use_od), 1024));
-    %S = StdWhiteS(S);
-    R = S2cov(S, use_od);
-    covz = R2covz(R);
-    covz_orig = covz;
+  S = A2S(A2d, De, max(pow2ceil(8*use_od), 1024));
+  %S = StdWhiteS(S);
+  R = S2cov(S, use_od);
+  covz = R2covz(R);
 
-    [GC De A2d] = RGrangerTfast(R);
-    bigR = covz_orig(p+1:end, p+1:end);
+  [GC De A2d] = RGrangerTfast(R);
+  bigR = covz(p+1:end, p+1:end);
+
+  id_bt2tb = reshape(1:p*m, p, [])'(:);
+  % prepare toeplitz block matrix
+  eR3 = bigR(id_bt2tb, id_bt2tb);
+  Q = inv(eR3);
+
+  function MatShow(A)
+      imagesc(A); colorbar; caxis([-1 1]*1e-3*max(abs(A(:))));
+  end
+
+  figure(11); MatShow(bigR);
+  figure(12); MatShow(inv(bigR));
+  figure(13); MatShow(eR3);
+  figure(14); MatShow(Q);
+
+  A3d = cat(3, eye(p), reshape(A2d,p,p,[]));
+  A3d(:,:,end) = [];
+  M = zeros(size(Q));
+  G = zeros(size(Q));
+  for k=0:m-1
+    M(k*p+1:(k+1)*p, k*p+1:end) = reshape(A3d(:,:,1:end-k), p, []);
+    G(k*p+1:(k+1)*p, k*p+1:(k+1)*p) = De;
+  end
+  iG = inv(G);
+
+  di = inv(bigR) - M'*iG*M;
+  figure(4); imagesc( di(1:end-fit_od*p, 1:end-fit_od*p) ); colorbar
+
+  M  = M (id_bt2tb, id_bt2tb);
+  iG = iG(id_bt2tb, id_bt2tb);
+
 end
 toc;
 
@@ -45,58 +75,25 @@ toc;
 id_x = 1;
 id_y = 2;
 
-id_bt2tb = reshape(1:p*m, p, [])'(:);
-
-gc_xy = expm1(GC(id_x, id_y))
-
-% prepare toeplitz block matrix
-erv3 = covz_orig(id_x, id_bt2tb+p);
-eR3 = bigR(id_bt2tb, id_bt2tb);
-
 id_bx = (1:m) + (id_x-1)*m;
 id_by = (1:m) + (id_y-1)*m;
 id_bz = 1:p*m;
 id_bz([id_bx id_by]) = [];
 
-Q = inv(eR3);
-
-function MatShow(A)
-  imagesc(A); colorbar; caxis([-1 1]*1e-3*max(abs(A(:))));
-end
-
-figure(11); MatShow(bigR);
-figure(12); MatShow(inv(bigR));
-figure(13); MatShow(eR3);
-figure(14); MatShow(Q);
-
+% a12 coef verification
+erv3 = covz(id_x, id_bt2tb+p);
 a12 = erv3 * Q(:, id_by);
 
 figure(1); plot(a12);
 err_coef = maxerr( a12 + A2d(id_x, id_y:p:end) )
 
+% GC verification
+gc_xy = expm1(GC(id_x, id_y))
 gc_xy_exact = a12 / Q(id_by, id_by) * a12' / De(1,1)
 err_gc_exact = maxerr(gc_xy - gc_xy_exact)
 
-% approximation
-
-A3d = cat(3, eye(p), reshape(A2d,p,p,[]));
-A3d(:,:,end) = [];
-M = zeros(size(Q));
-G = zeros(size(Q));
-for k=0:m-1
-  M(k*p+1:(k+1)*p, k*p+1:end) = reshape(A3d(:,:,1:end-k), p, []);
-  G(k*p+1:(k+1)*p, k*p+1:(k+1)*p) = De;
-end
-
-iG = inv(G);
-
-%di = inv(bigR) - M'*iG*M;
-%figure(2); imagesc( di(1:end-fit_od*p, 1:end-fit_od*p) ); colorbar
-
-M  = M (id_bt2tb, id_bt2tb);
-iG = iG(id_bt2tb, id_bt2tb);
-
-Qyy_mapp = M(:, id_by)'*iG*M(:, id_by);  % the approximation
+% GC approximation
+Qyy_mapp = M(:, id_by)'*iG*M(:, id_by);
 di = Q(id_by, id_by) - Qyy_mapp;
 
 figure(3); imagesc(di(1:end-fit_od, 1:end-fit_od)); colorbar
