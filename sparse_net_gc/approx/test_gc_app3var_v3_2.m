c7269adb352b7cd6e838cc58d5f54982cfa67803
xyy@xle <xyy82148@sjtu.edu.cn>
2016-03-18 01:26:24 +0400

    Add b12 app, but seems the pairwise GC is not the same.

--- sparse_net_gc/approx/test_gc_app3var_v3_2.m
+++ sparse_net_gc/approx/test_gc_app3var_v3_2.m
@@ -7,7 +7,7 @@ fit_od = 40;
 use_od = 120;
 m = use_od;
 
-ed = 0;
+%ed = 0;
 tic;
 if ~exist('ed', 'var') || isempty(ed) || ~ed
   ed = true;
@@ -50,11 +50,6 @@ if ~exist('ed', 'var') || isempty(ed) || ~ed
     imagesc(A); colorbar; caxis([-1 1]*err*max(abs(A(:))));
   end
 
-  figure(11); MatShow(bigR);
-  figure(12); MatShow(inv(bigR));
-  figure(13); MatShow(eR3);
-  figure(14); MatShow(Q);
-
   A3d = cat(3, eye(p), reshape(A2d,p,p,[]));
   A3d(:,:,end) = [];
   M = zeros(size(Q));
@@ -74,9 +69,73 @@ if ~exist('ed', 'var') || isempty(ed) || ~ed
 end
 toc;
 
-% analyse x <- y
-id_x = 1;
-id_y = 6;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% is inverse of covariance sparse
+
+if 0
+  figure(11); MatShow(bigR);
+  figure(12); MatShow(inv(bigR));
+  figure(13); MatShow(eR3);
+  figure(14); MatShow(Q);
+
+  % sum blocks of m*m
+  function arr = sumBlock(Q, m, p)
+    arr = permute(reshape(Q, m, p, m, p), [1 3 2 4]);
+    arr = squeeze(sum(reshape(abs(arr), m*m, p, p))) / m;
+    %arr(eye(p)==1) = 0;
+  end
+
+  arr= sumBlock(Q, m, p);
+
+  % Compare Q to adj
+  figure(5);
+  MatShow(arr, 2e-3);
+  figure(6);
+  adj = pm.net_adj*1;
+  MatShow(adj'+adj+eye(p), 0.1);
+
+  figure(7);
+  MatShow(arr, 5e-5);
+  figure(8);
+  adj_e = pm.net_adj*1 + eye(p);
+  MatShow(adj_e'*adj_e, 0.1);
+
+  % Verify M block to adj
+  figure(15);
+  MatShow(M, 0.001);
+  figure(16);
+  MatShow(adj_e, 0.001);
+  figure(17);
+  arr2 = sumBlock(M, m, p);
+  MatShow(arr2,0.02);
+  maxerr((arr2>1e-10) - (adj_e))
+
+  % Different level of Q
+  figure(21);
+  semilogy(sort(sort(arr(:))), '-o')
+  figure(22);
+  semilogy(sort(sort(sumBlock(M'*iG*M, m, p)(:))), '-o')
+
+  % Adj for different level of Q
+  net_h1 = zeros(p); net_h1(10<arr) = 1;
+  net_h2 = zeros(p); net_h2(0.1<arr & arr<10) = 1;
+  net_h3 = zeros(p); net_h3(1e-3<arr & arr<0.1) = 1;
+  net_h4 = zeros(p); net_h4(arr<1e-3) = 1;
+  figure(31);  MatShow(net_h1);
+  figure(32);  MatShow(net_h2);
+  figure(33);  MatShow(net_h3);
+  figure(34);  MatShow(net_h4);
+
+  % Guessed by adjacency matrix
+  figure(41);  MatShow(eye(p));
+  figure(42);  MatShow(adj'+adj);
+  figure(43);  MatShow(1*((1*(adj'*adj>0) - 1*((eye(p) + adj' + adj)>0))>0));
+end
+
+%%%%%%%%%%%%%%%%
+% analyse GC: x <- y
+id_x = 2;
+id_y = 7;
 
 id_bx = (1:m) + (id_x-1)*m;
 id_by = (1:m) + (id_y-1)*m;
@@ -104,57 +163,43 @@ figure(3); imagesc(di(1:end-fit_od, 1:end-fit_od)); colorbar
 gc_mapp = a12 / Qyy_mapp * a12' / De(id_x,id_x)
 err_gc_mapp = maxerr(gc_mapp - gc_xy)
 
-%% is inverse of covariance sparse
-
-% sum blocks of m*m
-function arr = sumBlock(Q, m, p)
-  arr = permute(reshape(Q, m, p, m, p), [1 3 2 4]);
-  arr = squeeze(sum(reshape(abs(arr), m*m, p, p))) / m;
-  %arr(eye(p)==1) = 0;
-end
-
-arr= sumBlock(Q, m, p);
-
-figure(5);
-MatShow(arr, 2e-3);
-figure(6);
-adj = pm.net_adj*1;
-MatShow(adj'+adj+eye(p), 0.1);
-
-figure(7);
-MatShow(arr, 5e-5);
-figure(8);
-adj_e = pm.net_adj*1 + eye(p);
-MatShow(adj_e'*adj_e, 0.1);
-
-figure(15);
-MatShow(M, 0.001);
-figure(16);
-MatShow(adj_e, 0.001);
-figure(17);
-arr2 = sumBlock(M, m, p);
-MatShow(arr2,0.02);
-maxerr((arr2>1e-10) - (adj_e))
-
-figure(21);
-semilogy(sort(sort(arr(:))), '-o')
-figure(22);
-semilogy(sort(sort(sumBlock(M'*iG*M, m, p)(:))), '-o')
-
-net_h1 = zeros(p); net_h1(10<arr) = 1;
-net_h2 = zeros(p); net_h2(0.1<arr & arr<10) = 1;
-net_h3 = zeros(p); net_h3(1e-3<arr & arr<0.1) = 1;
-net_h4 = zeros(p); net_h4(arr<1e-3) = 1;
-figure(31);  MatShow(net_h1);
-figure(32);  MatShow(net_h2);
-figure(33);  MatShow(net_h3);
-figure(34);  MatShow(net_h4);
-
-figure(41);  MatShow(eye(p));
-figure(42);  MatShow(adj'+adj);
-figure(43);  MatShow(1*((1*(adj'*adj>0) - 1*((eye(p) + adj' + adj)>0))>0));
-
-
 %%%%%%%%%%%%%%%
-% get pairwise GC coef
+% get pairwise GC
+
+R_xy = S2cov(S([id_x id_y],[id_x id_y],:), m);
+R_xy_ = R([id_x id_y], bsxfun(@plus, [id_x id_y]', 0:p:end-1));
+maxerr(R_xy - R_xy_)
+[pairGC D_B B] = RGrangerTfast(R_xy);
+gc_pair = expm1(pairGC(1,2))
+
+%%%%%%%%%%%%%%%%%
+% get pairwise coef
+
+a13_mapp = erv3 * Q(:, id_bz);
+
+b12 = -B(1, 2:2:end);
+figure(51); plot(b12);
+
+% the answer
+b12_Q = a12 - a13_mapp / Q(id_bz,id_bz) * Q(id_bz, id_by);
+% the approximation
+Qzz_mapp = M(:, id_bz)'*iG*M(:, id_bz);
+Qzy_mapp = M(:, id_bz)'*iG*M(:, id_by);
+b12_mapp = a12 - a13_mapp / Qzz_mapp * Qzy_mapp;
+err_b12_mapp = maxerr(b12 - b12_mapp)
+figure(52); plot(b12 - b12_mapp);
+
+%%%%%%%%%%%
+% gc app
+Qyz_mapp = M(:, id_by)'*iG*M(:, id_bz);
+% the answers
+P = inv(eR3([id_bx id_by], [id_bx id_by]));
+gc_pair_P = b12 / P(m+1:end, m+1:end) * b12'
+%gc_pair_Q = b12_mapp / (Q(id_by,id_by) - Q(id_by,id_bz)/Q(id_bz,id_bz)*Q(id_bz,id_by)) * b12_mapp'
+gc_pair_Q = b12 / (Q(id_by,id_by) - Q(id_by,id_bz)/Q(id_bz,id_bz)*Q(id_bz,id_by)) * b12'
+% the approximation
+gc_pair_mapp = b12_mapp / (Qyy_mapp - Qyz_mapp/Qzz_mapp*Qzy_mapp) * b12_mapp'
+gc_pair_mapp2 = b12_mapp / Qyy_mapp * b12_mapp'
+
+
 
