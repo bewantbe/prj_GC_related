048d0166359a386f556dfd54a009a89f8503fcde
xyy@xle <xyy82148@sjtu.edu.cn>
2016-03-31 01:02:58 +0400

    Fix load .rng file problem.

--- sparse_net_gc/worker/worker_data_checker.m
+++ sparse_net_gc/worker/worker_data_checker.m
@@ -14,7 +14,7 @@ s_sparseness = s_sparseness(ids_parted);
 func_name = 'worker_cell_GC_HH_VST';
 
 % for rng_state_curr
-load('scan_sparseness/scan_HH3_gcc49_westmere2_nogui_sparse=1.0e-03-9.3e-02_p=1000_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199485[1].mat.rng');
+load('scan_sparseness/scan_HH3_gcc49_westmere2_nogui_sparse=1.0e-03-9.3e-02_p=1000_pr=1.0_ps=3.0e-02_scee=5.0e-02_t=1.0e+06_2199485[1].mat.rng', '-mat');
 
 in_const_data.no_postprocess = true;
 
