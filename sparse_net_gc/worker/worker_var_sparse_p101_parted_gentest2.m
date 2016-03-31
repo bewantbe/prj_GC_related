15a39452d5463cdd7677bbee1eee06db9ebda882
xyy@xle <xyy82148@sjtu.edu.cn>
2016-03-31 21:35:13 +0400

    update

--- sparse_net_gc/worker/worker_var_sparse_p101_parted_gentest2.m
+++ sparse_net_gc/worker/worker_var_sparse_p101_parted_gentest2.m
@@ -1,14 +1,14 @@
 %
 
 identity_str='test101';
-ids_parted = 1:11;
+ids_parted = 1:22;
 in_const_data.no_postprocess = true;
 
 
 simu_time = 1e5;
 p = 101;
 n_indirect = 10;
-s_sparseness = linspace(1, round(sqrt(p*n_indirect)), 11)/p;
+s_sparseness = linspace(1, round(sqrt(p*n_indirect)), 22)/p;
 ncpu = 11;
 
 s_sparseness = s_sparseness(ids_parted);
