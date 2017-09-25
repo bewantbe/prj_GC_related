41372232b84cf31360c4ee863e289a005ca48e96
Eddy Xiao <bewantbe@gmail.com>
2017-09-26 07:01:12 +0800

    Add gain scanner HH-GH VT=65.

---
+++ HH_reboot/scan_ISI_prps_ps_mV_HHGH_VT65_w2.m
@@ -0,0 +1,19 @@
+% compute firing freq
+addpath([getenv('HOME') '/code/point-neuron-network-simulator/mfile/']);
+
+neuron_model = 'HH-GH';
+extra_cmd = '--set-threshold 65';
+simu_time = 1e7;
+
+s_ps_mV = [...
+0.05, 0.07, 0.1, 0.14, 0.2, 0.3, 0.5, 0.7, 1.0, 1.4, 2.0...
+];
+
+s_prps_mV = linspace(0.1, 15, 100);
+
+prefix_tmpdata = 'ISI_results_HHGH/';
+name_suffix = '_VT=65';
+
+for ps_mV = s_ps_mV
+  scan_ISI_prps_ps_mV_head;
+end
