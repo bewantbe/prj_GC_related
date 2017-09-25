e78f2c82e3def0261d8053852bfa4d7b545f2ac4
Eddy Xiao <bewantbe@gmail.com>
2017-09-26 07:54:20 +0800

    Correction.

--- HH_reboot/scan_ISI_prps_ps_mV_HHG_VT65_w2.m
+++ HH_reboot/scan_ISI_prps_ps_mV_HHG_VT65_w2.m
@@ -2,7 +2,7 @@
 addpath([getenv('HOME') '/code/point-neuron-network-simulator/mfile/']);
 
 neuron_model = 'HH-G';
-extra_cmd = '--set-threshold 20';
+extra_cmd = '--set-threshold 65';
 simu_time = 1e7;
 
 s_ps_mV = [...
@@ -12,7 +12,7 @@ s_ps_mV = [...
 s_prps_mV = linspace(0.1, 15, 100);
 
 prefix_tmpdata = 'ISI_results_HHG/';
-name_suffix = '_VT=20';
+name_suffix = '_VT=65';
 
 for ps_mV = s_ps_mV
   scan_ISI_prps_ps_mV_head;
